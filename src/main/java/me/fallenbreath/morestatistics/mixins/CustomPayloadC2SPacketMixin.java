1738d12b89c152be30e5935d11db792d7ade0375
Fallen_Breath <yqx791125833@gmail.com>
2020-07-11 18:02:42 +0800

    Revert "1.15.2"
    This reverts commit 4d278a6295393079c37c15b76b65b5ca6ec784c8.

--- src/main/java/me/fallenbreath/morestatistics/mixins/CustomPayloadC2SPacketMixin.java
+++ src/main/java/me/fallenbreath/morestatistics/mixins/CustomPayloadC2SPacketMixin.java
@@ -1,9 +1,9 @@
 package me.fallenbreath.morestatistics.mixins;
 
 import me.fallenbreath.morestatistics.interfaces.ICustomPayloadC2SPacket;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.PacketByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
