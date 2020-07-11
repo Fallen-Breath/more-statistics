9dd449d9fcbb31d1388a1f30676f26dadea7e78c
Fallen_Breath <yqx791125833@gmail.com>
2020-07-11 17:43:06 +0800

    1.0.1

--- src/main/java/me/fallenbreath/morestatistics/MoreStatistics.java
+++ src/main/java/me/fallenbreath/morestatistics/MoreStatistics.java
@@ -8,6 +8,6 @@ public class MoreStatistics
 {
 	public static final String name = "More Statistics";
 	public static final String id = name.toLowerCase().replace(' ', '-');
-	public static final String version = "1.0.0";  // should be the same as the version in gradlew.properties
+	public static final String version = "1.0.1";  // should be the same as the version in gradlew.properties
 	public static final Logger logger = LogManager.getLogger();
 }
